8ecfef76dbf78002742ef48ec9174321e5fdfd8c
kimlah <wil11070@byui.edu>
2020-12-10 10:37:54 -0800

    big update

--- Scoots/js/rentals.js
+++ Scoots/js/rentals.js
@@ -1,4 +1,4 @@
-const file = open('rentals.json',)
+const file = './data/rentals.json'
 
 fetch(file)
     .then(function (response) {
@@ -13,31 +13,33 @@ fetch(file)
             let card = document.createElement("section");
 
             let h2 = document.createElement("h2");
-            h2.textContent = vehicle[i].type;
+            h2.textContent = vehicle[i].make;
             card.appendChild(h2);
+            document.querySelector("div.vehicle-cards").appendChild(card);
 
-            let h3 = document.createElement("h2");
-            h3.textContent = vehicle[i].make;
+            let picture = document.createElement("img");
+            picture.setAttribute("src", "../images." + vehicle[i].picture);
+            picture.setAttribute("alt", `Photo of ${vehicle[i].make}`);
+            card.appendChild(picture);
+
+            let h3 = document.createElement("h3");
+            h3.textContent = "Extra Information: " + vehicle[i].description + ", Seats " + vehicle[i].seats;
             card.appendChild(h3);
 
             let p1 = document.createElement("p");
-            p1.textContent = vehicle[i].max-people;
+            p1.textContent = "Reservation Half Day: " + vehicle[i].reservation.halfday;
             card.appendChild(p1);
 
             let p2 = document.createElement("p");
-            p2.textContent = vehicle[i].reservation.half-day;
+            p2.textContent = "Reservation Full Day: " + vehicle[i].reservation.fullday;
             card.appendChild(p2);
 
             let p3 = document.createElement("p");
-            p3.textContent = vehicle[i].reservation.full-day;
+            p3.textContent = "Walk in Half Day: " + vehicle[i].walkIn.halfday;
             card.appendChild(p3);
 
             let p4 = document.createElement("p");
-            p4.textContent = vehicle[i].walkIn.half-day;
+            p4.textContent = "Walk in Full Day: " + vehicle[i].walkIn.fullday;
             card.appendChild(p4);
-
-            let p5 = document.createElement("p");
-            p5.textContent = vehicle[i].walkIn.full-day;
-            card.appendChild(p5);
         }
     });
--- Scoots/js/weather.js
+++ Scoots/js/weather.js
@@ -5,58 +5,30 @@ const weatherAPIurl = APIurl + 'weather';
 const forecastAPIurl =  APIurl + 'forecast';
 const ID = '&appid=cc326e22c40b514d2512902cd32e0862';
 const APIunits = '&units=imperial';
-const preston = '?id=5604473' + ID + APIunits;
-const sodasprings = '?id=5607916' + ID + APIunits;
-const fishhaven = '?lat=42.0380399&lon=-111.4048681' + ID + APIunits;
+const cozumel = '?id=3530103' + ID + APIunits;
 
 /* Concatanated Links */
-    // Preston
-const prestonForecast = forecastAPIurl + preston;
-const prestonWeather = weatherAPIurl + preston;
-    // Soda Springs
-const sodaspringsForecast = forecastAPIurl + sodasprings;
-const sodaspringsWeather = weatherAPIurl + sodasprings;
-    // Fish Haven
-const fishhavenForecast = forecastAPIurl + fishhaven;
-const fishhavenWeather = weatherAPIurl + fishhaven;
+    // Cozumel
+const cozumelForecast = forecastAPIurl + cozumel;
+const cozumelWeather = weatherAPIurl + cozumel;
 
 /* Choosing which page needs weather and events */
 let weatherURL = "";
 let forecastURL = "";
 
-if (document.getElementById("preston")) {
-    weatherURL = prestonWeather;
-    forecastURL = prestonForecast;
-}
-else if (document.getElementById("soda-springs")) {
-    weatherURL = sodaspringsWeather;
-    forecastURL = sodaspringsForecast;
-}
-else if (document.getElementById("fish-haven")) {
-    weatherURL = fishhavenWeather;
-    forecastURL = fishhavenForecast;
-}
+document.getElementById("cozumel"); 
+    weatherURL = cozumelWeather;
+    forecastURL = cozumelForecast;
 
 /* Weather Function */
 fetch(weatherURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        let t = parseFloat(jsObject.main.temp);
-        let s = parseFloat(jsObject.wind.speed);
-        let output = "N/A";
 
         const desc = jsObject.weather[0].description; 
         document.getElementById('current').innerHTML = jsObject.weather[0].description;
         document.getElementById('currentTemp').innerHTML = Math.round(t) + "&#8457;";
-
-        if (t <= 50 && s >= 3) {
-            let f = (35.74 + (0.6215 * t)) - (35.75 * (Math.pow(s, 0.16))) + (0.4275 * (t * (Math.pow(s, 0.16))));
-            output = Math.round(f);
-        };
-
-        document.getElementById("windChill").innerHTML = output + "&#8457;";
         document.getElementById('humidity').innerHTML = jsObject.main.humidity;
-        document.getElementById('windSpeed').innerHTML = Math.round(s) + " MPH";
     });
 
 /* Forecast Function */
@@ -90,24 +62,3 @@ fetch(forecastURL)
             }
         }
     });
-
-/* WINDCHILL */
-        /* The formula to calculate the wind chill factor is 
-        f=35.74+0.6215t−35.75s0.16+0.4275ts0.16, 
-        where f is the wind chill factor in Fahrenheit, 
-        t is the air average temperature in Fahrenheit, 
-        and s is the wind speed in miles per hour. */
-
-function getWindChill(temp, windSpeed) {
-   if ((temp <= 50) && (windSpeed > 3.0)) {
-      var windChill = (35.74 + (0.6215 * temp) - 
-      (35.75 * (Math.pow(windSpeed, 0.16))) + 
-      (0.4275 * temp * (Math.pow(windSpeed, 0.16)))).toFixed(0);
-   }
-   else {
-      var windChill = "N/A";
-   }
-   return windChill;
-}
-
-document.getElementById("windChill").textContent = getWindChill(currentTemp, windSpeed);
